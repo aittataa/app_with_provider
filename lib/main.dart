84cf18aee52ccba71e4d03003eb3b60a4ae5a477
ABDERRAHIM AIT TATA <aittataa@gmail.com>
2021-12-18 19:15:19 +0100

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,48 +1,24 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:provider/provider.dart';
-import 'package:wallet_manager/constant/constant.dart';
-import 'package:wallet_manager/constant/messages.dart';
-import 'package:wallet_manager/provider/categories_provider.dart';
-import 'package:wallet_manager/provider/settings_provider.dart';
-import 'package:wallet_manager/provider/transactions_provider.dart';
-import 'package:wallet_manager/screens/initial_screen.dart';
+import 'package:get/get.dart';
+import 'package:wallet_online/app/config/functions/app_function.dart';
+
+import 'app/routes/app_pages.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
-  SystemChrome.setSystemUIOverlayStyle(
-    const SystemUiOverlayStyle(
-      systemNavigationBarColor: whiteBackColor,
-      systemNavigationBarIconBrightness: Brightness.dark,
-      statusBarColor: transparentColor,
-      statusBarIconBrightness: Brightness.dark,
-    ),
-  );
-  runApp(const WalletManager());
+  AppFunction.configureDependencies;
+  runApp(const WalletOnline());
 }
 
-class WalletManager extends StatelessWidget {
-  const WalletManager({Key key}) : super(key: key);
+class WalletOnline extends StatelessWidget {
+  const WalletOnline({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        ChangeNotifierProvider.value(value: SettingsProvider()),
-        ChangeNotifierProvider.value(value: CategoriesProvider()),
-        ChangeNotifierProvider.value(value: TransactionsProvider()),
-      ],
-      builder: (context, child) {
-        return const MaterialApp(
-          debugShowCheckedModeBanner: false,
-          title: Messages.appTitle,
-          color: mainColor,
-          home: InitialScreen(),
-          // home: Splash(),
-        );
-      },
+    return GetMaterialApp(
+      title: "Application",
+      initialRoute: AppPages.INITIAL,
+      getPages: AppPages.routes,
     );
   }
 }
