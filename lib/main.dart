1ee960234488bfbc419febe74e0155511815e57b
ABDERRAHIM AIT TATA <aittataa@gmail.com>
2021-12-21 18:27:49 +0100

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,48 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import 'package:wallet_online/app/config/functions/app_function.dart';
-import 'package:wallet_online/app/config/messages/app_message.dart';
-import 'package:wallet_online/app/config/themes/app_theme.dart';
-import 'package:wallet_online/app/routes/app_pages.dart';
+import 'package:flutter/services.dart';
+import 'package:provider/provider.dart';
+import 'package:wallet_manager/constant/constant.dart';
+import 'package:wallet_manager/constant/messages.dart';
+import 'package:wallet_manager/provider/categories_provider.dart';
+import 'package:wallet_manager/provider/settings_provider.dart';
+import 'package:wallet_manager/provider/transactions_provider.dart';
+import 'package:wallet_manager/widget/splash.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  AppFunction.configureDependencies;
-  runApp(const WalletOnline());
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+  SystemChrome.setSystemUIOverlayStyle(
+    const SystemUiOverlayStyle(
+      systemNavigationBarColor: whiteBackColor,
+      systemNavigationBarIconBrightness: Brightness.dark,
+      statusBarColor: transparentColor,
+      statusBarIconBrightness: Brightness.dark,
+    ),
+  );
+  runApp(const WalletManager());
 }
 
-class WalletOnline extends StatelessWidget {
-  const WalletOnline({Key? key}) : super(key: key);
+class WalletManager extends StatelessWidget {
+  const WalletManager({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: AppMessage.appTitle,
-      theme: AppTheme.themeData,
-      getPages: AppPages.routes,
-      initialRoute: AppPages.INITIAL,
-      // home: Splash(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider.value(value: SettingsProvider()),
+        ChangeNotifierProvider.value(value: CategoriesProvider()),
+        ChangeNotifierProvider.value(value: TransactionsProvider()),
+      ],
+      builder: (context, child) {
+        return const MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: Messages.appTitle,
+          color: mainColor,
+          // home: InitialScreen(),
+          home: Splash(),
+        );
+      },
     );
   }
 }
